fef3b9f9e87769e05ff2086526d3162978538241
Xsimple <aderval758@gmail.com>
2023-12-02 19:38:58 -0300

    move the content of Video.cpp to wflGl.cpp

--- include/WFL_GL/wflGl.hpp
+++ include/WFL_GL/wflGl.hpp
@@ -1,14 +1,13 @@
-#ifndef VIDEO_H
-#define VIDEO_H
+#ifndef WFL_GL_h
+#define WFL_GL_h
 
 #include "SDL2/SDL.h"
 #include "glad/glad.h"
 #include "LibretroClass.hpp"
 #include "debug.hpp"
 #include "videoDefs.hpp"
-#include "WFL_GL/wflGl.hpp"
 
-class VideoClass {
+class WFLGlClass {
 	public:
 		void init(libretro_external_data* data, retro_game_geometry* geometry);
 		bool setPixelFormat(unsigned format, libretro_external_data* data);
@@ -16,7 +15,10 @@ class VideoClass {
 		void resizeToAspect(double ratio, int sw, int sh, int* dw, int* dh);
 		void videoRefresh(const void* data, unsigned width, unsigned height, unsigned pitch);
 		void deinit();
-	
+	private:
+		void createWindow(int width, int height);
+		void initShader();
+		GLuint compileShader(unsigned type, unsigned count, const char** strings);
 };
 
 #endif
